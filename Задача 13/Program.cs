2de6152a88cbf87acbc059fff36d7c26468d32fb
Vlad <rkt9021@mail.ru>
2022-09-19 20:58:19 +0300

    Добавили файлы для работы в задачу 13 и написали код

---
+++ Задача 13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(number);
+if(result.Length > 2)
+{
+    Console.WriteLine("Третье число это: " + result[2]);
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
