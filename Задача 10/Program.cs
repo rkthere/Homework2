4a6ae9de31e3bdefa528d1971e3a2a5c091b763c
Vlad <rkt9021@mail.ru>
2022-09-19 20:51:48 +0300

    Добавили рабочую область и код к задаче 10

---
+++ Задача 10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string result = Convert.ToString(number);
+Console.WriteLine("Второе число это: " + result[1]);
