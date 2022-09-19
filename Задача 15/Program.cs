3ba66daf8792e91400707868287f9b51e93d5f55
Vlad <rkt9021@mail.ru>
2022-09-19 21:07:50 +0300

    Добавили файлы для работы и написали код

---
+++ Задача 15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+if(dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+else
+{
+    Console.WriteLine("Это не выходной :(");
+}
